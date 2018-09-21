6ca23a6e3179602bdb00ef0a2f05b6fe1499c773
ldorilas <43239931+ldorilas@users.noreply.github.com>
2018-09-21 11:50:50 -0400

    Add files via upload

---
+++ ProblemSet1.java
@@ -0,0 +1,30 @@
+/**
+ * INSTRUCTIONS.
+ * 
+ * Problem Set 1 will test your skills of printing to the console. You'll
+ * be asked to print various text, shapes, and figures, all of which is
+ * explained (and demonstrated) in detail in the README file.
+ */
+
+public class ProblemSet1 {	
+	
+	/**
+	 * Every Java program must have a main method, which is
+	 * where program execution begins.
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+		System.out.println("   ##    ######  ######## ########\n  #  #   #     # #        #\n #    #  #     # #        #\n######## ######  #        ########\n#      # #       #               #\n#      # #       #               #\n#      # #       ######## ########\n");
+		
+		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+		System.out.println("  /\\     /\\     /\\ \n /  \\   /  \\   /  \\ \n/    \\ /    \\ /    \\ \n\\    / \\    / \\    / \n \\  /   \\  /   \\  / \n  \\/     \\/     \\/ \n");
+
+		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
+		System.out.print("\t \t \\\\\\\\\\\n\t\t+\"\"\"\"\"+\n\t      (|  0 0  |)\n\t       |   ^   |\n\t       |  \'-\'  |\n\t\t+-----+\n\n");
+		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
+		System.out.println("\t\tH E L L O\n\t\tE\tA\n\t\tL\tP\n\t\tL\tC\n\t\tO A P C S\n");
+		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		System.out.println("\"The answer\'s quite simple,\" the professor said. \"Just think outside the box!\"");
+	}
+}
